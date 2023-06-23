8d22946664686633f4042fa8a05d05f4961c7e20
Pavitra Kumar Nayak <pavitrakumarnayak@Pavitras-MacBook-Air.local>
2023-06-23 19:31:35 +0530

    first commit - project setup

---
+++ src/component/Chat.js
@@ -0,0 +1,9 @@
+import React, { Component, useState } from 'react';
+
+function Chat() {
+    const [color, setColor] = useState("primary");
+    return ( <div>
+hello    </div>);
+}
+
+export default Chat;
